c845dbf814e1ceb983ad1e648b528ba4ec392445
Eryko Costa <eryko4350@gmail.com>
2026-09-21 17:11:37 -0300

    imagem app securitario

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ load_dotenv()
 # ---------------------------------------------------------------------------
 st.set_page_config(
     page_title="Agente Jurídico Securitário | Dra. Salviana Lima",
-    page_icon="⚖️",
+    page_icon="assets/favicon.jpg",
     layout="wide",
     initial_sidebar_state="expanded",
     menu_items={
